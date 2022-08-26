e151d0f3644569aae765b9960976df9db778fb84
lynguyen <nly36524@gmail.com>
2022-08-26 12:24:13 +0700

    upload source code

---
+++ admin/includes/footer.php
@@ -0,0 +1,6 @@
+<footer class="main-footer">
+    <div class="pull-right hidden-xs">
+      <b>All rights reserved</b>
+    </div>
+     <strong>20 Đ. Cộng Hòa, Phường 12, Tân Bình, TP Hồ Chí Minh</strong>
+</footer>
---
+++ includes/footer.php
@@ -0,0 +1,8 @@
+<footer class="main-footer">
+    <div class="container">
+      <div class="pull-right hidden-xs">
+        <b>All rights reserved</b>
+      </div>
+      <strong>20 Đ. Cộng Hòa, Phường 12, Tân Bình, TP Hồ Chí Minh</strong>
+    </div>
+</footer>
